2450572e0cf88fb4421affa0e542f0c2bd0fff8a
Kevin Alarcon <kevinarbey98@gmail.com>
2024-06-10 22:02:53 -0500

    AparatosElectronicos

---
+++ AparatosElectronicos/src/main/java/uptc/frw/aparatoselectronicos/AparatosElectronicosApplication.java
@@ -0,0 +1,14 @@
+package uptc.frw.aparatoselectronicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AparatosElectronicosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AparatosElectronicosApplication.class, args);
+        System.out.println("hola mundo cruel");
+    }
+
+}
