314fc3a03250f22ae4b389ddc7ca8e8c80efbefc
Kevin Alarcon <kevinarbey98@gmail.com>
2024-06-11 20:10:36 -0500

    kevin v2

---
+++ AparatosElectronicos/src/main/java/uptc/frw/aparatoselectronicos/jpa/repository/ElectronicDeviceRepository.java
@@ -0,0 +1,6 @@
+package uptc.frw.aparatoselectronicos.jpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ElectronicDeviceRepository extends JpaRepository<ElectronicDeviceRepository, Long> {
+}
