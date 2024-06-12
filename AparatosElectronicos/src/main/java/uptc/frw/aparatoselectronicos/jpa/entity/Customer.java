ba66973d1dc565e99e3b4f7f97531902baef9190
Felipe Chaparro Pirajón <ichaparropirajon@gmail.com>
2024-06-11 21:50:59 -0500

    Creación entidades: RepairComponent & Customer

--- AparatosElectronicos/src/main/java/uptc/frw/aparatoselectronicos/jpa/entity/Customer.java
+++ AparatosElectronicos/src/main/java/uptc/frw/aparatoselectronicos/jpa/entity/Customer.java
@@ -1,4 +1,103 @@
 package uptc.frw.aparatoselectronicos.jpa.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "CLIENTE")
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_CLIENTE")
+    private Long id;
+
+    @Column(name = "NOMBRE_CLIENTE")
+    private String firstName;
+
+    @Column(name = "APELLIDO_CLIENTE")
+    private String lastName;
+
+    @Column(name = "NUMERO_DOCUMENTO")
+    private String documentNumber;
+
+    @Column(name = "DIRECCION")
+    private String address;
+
+    @Column(name = "TELEFONO")
+    private String phone;
+
+    @Column(name = "CORREO_ELECTRONICO")
+    private String email;
+
+    public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(String documentNumber) {
+        this.documentNumber = documentNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", documentNumber='" + documentNumber + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
--- AparatosElectronicos/src/main/java/uptc/frw/aparatoselectronicos/jpa/entity/RepairComponent.java
+++ AparatosElectronicos/src/main/java/uptc/frw/aparatoselectronicos/jpa/entity/RepairComponent.java
@@ -1,4 +1,62 @@
 package uptc.frw.aparatoselectronicos.jpa.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "REPARACIONCOMPONENTE")
 public class RepairComponent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_REPARACIONCOMPONENTE")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_REPARACION")
+    private Repair repair;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_COMPONENTE")
+    private Component component;
+
+    public RepairComponent() {
+    }
+
+    public RepairComponent(Repair repair, Component component) {
+        this.repair = repair;
+        this.component = component;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Repair getRepair() {
+        return repair;
+    }
+
+    public void setRepair(Repair repair) {
+        this.repair = repair;
+    }
+
+    public Component getComponent() {
+        return component;
+    }
+
+    public void setComponent(Component component) {
+        this.component = component;
+    }
+
+    @Override
+    public String toString() {
+        return "RepairComponent{" +
+                "id=" + id +
+                ", repair=" + repair +
+                ", component=" + component +
+                '}';
+    }
 }
