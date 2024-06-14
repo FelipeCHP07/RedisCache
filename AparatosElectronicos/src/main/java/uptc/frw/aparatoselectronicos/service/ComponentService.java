efce568173873d95c232475e0e496842f9ea665a
Felipe Chaparro Pirajón <ichaparropirajon@gmail.com>
2024-06-13 22:33:51 -0500

    Update ComponentService.java

--- AparatosElectronicos/src/main/java/uptc/frw/aparatoselectronicos/service/ComponentService.java
+++ AparatosElectronicos/src/main/java/uptc/frw/aparatoselectronicos/service/ComponentService.java
@@ -1,17 +1,1 @@
-package uptc.frw.aparatoselectronicos.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import uptc.frw.aparatoselectronicos.jpa.entity.Component;
-import uptc.frw.aparatoselectronicos.jpa.repository.ComponentRepository;
-
-@Service
-public class ComponentService {
-
-    @Autowired
-    private ComponentRepository componentRepository;
-
-    public Component getComponentById(long id) {
-        return componentRepository.findById(id).orElse(null);
-    }
-}
